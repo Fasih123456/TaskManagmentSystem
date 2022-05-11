2de52c748c38e9d06f6e047df4c26a689e6d6515
Fasih <fasihislam1999@gmail.com>
2022-05-11 13:59:35 -0300

    Finished creating Modals

---
+++ API/TaskTrackingAPI/TaskTrackingAPI/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskTrackingAPI.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+
+        public string TaskDate { get; set; }
+
+        public string Day { get; set; }
+
+        public string TaskName { get; set; }
+
+        public int TimeRequired { get; set; }
+
+        public int TimeNotSpent { get; set; }
+
+        public string TaskStatus { get; set; }
+    }
+}
